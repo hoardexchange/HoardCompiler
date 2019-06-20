79244e9246392fa16bb0dc73565711802f162960
edwarf <cyryl.matuszewski@gmail.com>
2019-06-20 23:06:32 +0200

    [GOL-2] [GOL-4] GolemCompilerVSIX extension package for Visual Studio created

---
+++ GolemCompilerVSIX/BuildCommand.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace GolemCompilerVSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class BuildCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+        public const int SlnCommandId = 0x0101;
+        public const int ContextCommandId = 0x0102;
+        public const int SlnContextCommandId = 0x0103;
+
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("4a0e73c2-8f07-44b9-837a-3f7db6209609");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private BuildCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                menuCommandID = new CommandID(CommandSet, SlnCommandId);
+                menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                menuCommandID = new CommandID(CommandSet, ContextCommandId);
+                menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                menuCommandID = new CommandID(CommandSet, SlnContextCommandId);
+                menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static BuildCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new BuildCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "BuildCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ GolemCompilerVSIX/GolemCompilerPackage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace GolemCompilerVSIX
+{
+	public class OptionPageGrid : DialogPage
+	{
+        [Category("GolemCompiler")]
+        [DisplayName("url to Golem Hub")]
+        [Description("Can be used to specify the url to Golem Hub")]
+        public string OptionGolemHubUrl
+        {
+            get;
+            set;
+        } = "localhost:8080";
+
+        [Category("GolemCompiler")]
+        [DisplayName("Use unity files")]
+        [Description("Whether to attempt to use 'unity' files to speed up compilation. May require modifying some headers.")]
+        public bool OptionUseUnity
+        {
+            get;
+            set;
+        } = false;
+	}
+
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "0.1", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(GolemCompilerPackage.PackageGuidString)]
+    [ProvideOptionPage(typeof(OptionPageGrid), "Golem Compiler", "Options", 0, 0, true)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class GolemCompilerPackage : Package
+    {
+        /// <summary>
+        /// GolemCompilerPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "58caa410-0099-4620-a7b4-8cce182d193b";
+
+		public IVsOutputWindowPane m_outputPane;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GolemCompilerPackage"/> class.
+        /// </summary>
+        public GolemCompilerPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            BuildCommand.Initialize(this);
+
+            var outputWindow = GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            if (outputWindow == null) return;
+
+            var outputPaneId = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+            var hresult = outputWindow.CreatePane(outputPaneId, "GolemCompiler", 1, 0);
+            ErrorHandler.ThrowOnFailure(hresult);
+
+            hresult = outputWindow.GetPane(outputPaneId, out m_outputPane);
+            ErrorHandler.ThrowOnFailure(hresult);
+
+            m_outputPane.Activate();
+            m_outputPane.OutputString("Golem Compiler extension initialized and ready to run!");
+        }
+
+        #endregion
+    }
+}
