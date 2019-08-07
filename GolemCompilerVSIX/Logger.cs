1c64f22f24e9b6de14b942ca62c894a22f6fc54f
ed_warf <cyryl.matuszewski@gmail.com>
2019-08-07 12:28:07 +0200

    [GOL-27] mising file

---
+++ GolemCompilerVSIX/Logger.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Threading;
+using System;
+
+namespace GolemCompiler
+{
+    internal static class Logger
+    {
+        private static string _name;
+        private static IVsOutputWindowPane _pane;
+        private static IVsOutputWindow _output;
+
+        public static async System.Threading.Tasks.Task InitializeAsync(AsyncPackage package, string name)
+        {
+            await package.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            _output = await package.GetServiceAsync(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            _name = name;
+            if (_output != null)
+            {
+                Guid guid = Guid.NewGuid();
+                _output.CreatePane(ref guid, _name, 1, 1);
+                _output.GetPane(ref guid, out _pane);
+            }            
+        }
+
+        public static void Log(object message)
+        {
+            try
+            {
+                if (_pane!=null)
+                {
+                    //TODO: maybe this should be called as a Task or Inovke or sth to ensure it is called on the correct thread!
+                    _pane.OutputString(DateTime.Now.ToString() + ": " + message + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex);
+            }
+        }
+    }
+}
